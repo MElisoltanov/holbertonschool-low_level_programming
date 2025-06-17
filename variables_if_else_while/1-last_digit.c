39bd665312600d043253f74d886d532a85b4fd96
MElisoltanov <elisoltanov.moussa@outlook.fr>
2025-06-17 16:05:55 +0200

    Print last digit and describe its value

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -14,14 +14,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	lastDigit = n % 10;
+	lastDigit = (n % 10);
 
 	if (lastDigit > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 	else if (lastDigit == 0)
 		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 	else
-		printf("Last digit of %d is %d and is less than 6 ", n, lastDigit);
-	printf("and not 0\n");
+		printf("Last digit of %d is %d and is less than 6 and not 0\n",
+				n, lastDigit);
 	return (0);
 }
