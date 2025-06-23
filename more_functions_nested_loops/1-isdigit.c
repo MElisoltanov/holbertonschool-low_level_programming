ff9c22f5982505685ec03497636e99a9b8d15696
MElisoltanov <elisoltanov.moussa@outlook.fr>
2025-06-23 10:16:09 +0200

    Implement _isdigit function to check for digit characters

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <unistd.h>
+#include <ctype.h>
+/**
+ * _isalpha - checks for letter
+ * @c: the character to check
+ *
+ * Return: 1 if c is letter  lowercase or uppercase, 0 otherwise
+ */
+
+int _isdigit(int c)
+
+{
+	if (c >= '0' && c <= '0')
+	{
+		return (1);
+	}
+
+	else
+
+	{
+		return (0);
+	}
+}
