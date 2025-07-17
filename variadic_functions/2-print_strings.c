d5d52eea400180250228413214fe86591fd58868
MElisoltanov <elisoltanov.moussa@outlook.fr>
2025-07-17 13:10:54 +0200

    Implement print_strings function to print strings with separator and handle NULL values

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_strings - Prints strings followed by a new line.
+ * @separator: String to be printed between the strings
+ * @n: Number of strings passed to the function
+ *
+ * Description: Prints each string from the variadic list.
+ * If a string is NULL, prints "(nil)" instead.
+ * If separator is not NULL, it is printed between strings,
+ * but not after the last one.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i = 0;
+	char *result;
+
+	va_start(args, n);
+
+	while (i < n)
+	{
+		result = va_arg(args, char *);
+
+		printf("%s", result);
+
+		if (separator == NULL)
+		{
+			continue;
+		}
+
+		if (n == 0)
+		{
+			printf("(nil)");
+		}
+
+		if (i < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+		i++;
+	}
+	printf("\n");
+}
