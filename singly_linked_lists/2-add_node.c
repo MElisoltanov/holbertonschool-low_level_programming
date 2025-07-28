2e26aa2b286747c8e6066b79d135e63e1598e71c
MElisoltanov <elisoltanov.moussa@outlook.fr>
2025-07-28 21:02:34 +0200

    Implement add_node function to prepend new node to list_t list

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,28 +11,20 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	
-	/* Étape 1 : Allouer de la mémoire pour un nouveau nœud */
 	list_t *new_node = malloc(sizeof(list_t));
 
-    /* Étape 2 : Vérifier si malloc a échoué */
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
 
-    /* Étape 3 : Dupliquer la chaîne str avec strdup */
 	new_node->str = strdup(str);
 
-    /* Étape 4 : Calculer la longueur de la chaîne (avec strlen) et la stocker */
 	new_node->len = strlen(str);
 
-    /* Étape 5 : Relier le nouveau nœud à l'ancien head */
 	new_node->next = *head;
 
-    /* Étape 6 : Mettre à jour head pour pointer vers le nouveau nœud */
 	*head = new_node;
 
-    /* Étape 7 : Retourner l'adresse du nouveau nœud */
 	return (new_node);
 }
