a1c4231a7a56d9f79e4085cbd8b23b8aa0ab5ff8
MElisoltanov <elisoltanov.moussa@outlook.fr>
2025-07-30 10:41:50 +0200

    Implement free_list function to free all nodes in list_t

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "lists.h" 
+
+/**
+ * free_list - Frees a list_t list
+ * @head: Pointer to the first node in the list
+ *
+ * Return: Nothing (void)
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head->next;
+
+		free(head->str);
+
+		free(head);
+
+		head = tmp;
+
+	}
+}
